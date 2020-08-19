e26648202e16998c108050fcd3065fa2759b1c24
Zhijie He <hezhijie0327@hotmail.com>
2020-08-19 23:45:23 +0800

    aria2.sh | Version 1.0.1
    1. <Change> Fix some errors.

--- aria2.sh
+++ aria2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Current Version: 1.0.0
+# Current Version: 1.0.1
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/aria2.conf.git" && chmod 0777 ./aria2.conf/aria2.sh && bash ./aria2.conf/aria2.sh
 
-aria2c_pid=$(ps -ef | grep "aria2c" | grep -v "grep" | awk '{print $2}')
+aria2c_pid=$(ps -ef | grep "aria2c" | grep -v "grep" | awk '{ print $2 }')
 
 if [ "${aria2c_pid}" == "" ]; then
     if [ ! -f "/etc/aria2/aria2.session" ]; then
