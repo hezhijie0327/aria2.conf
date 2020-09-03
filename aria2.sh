0bea0191e78015c71f3ed3043a358c66753e167e
Zhijie He <hezhijie0327@hotmail.com>
2020-09-03 14:39:20 +0800

    aria2.sh | Version 1.0.3
    1. <Change> Optimize the logic of environment initialization.

--- aria2.sh
+++ aria2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.0.2
+# Current Version: 1.0.3
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/aria2.conf.git" && chmod 0777 ./aria2.conf/aria2.sh && bash ./aria2.conf/aria2.sh
@@ -22,6 +22,9 @@ if [ "${aria2c_pid}" == "" ]; then
     fi
     if [ ! -f "/etc/aria2/aria2.log" ]; then
         touch "/etc/aria2/aria2.log"
+    fi
+    if [ ! -f "/etc/aria2/aria2.netrc" ]; then
+        touch "/etc/aria2/aria2.netrc"
     fi
     if [ ! -f "/etc/aria2/aria2.session" ]; then
         touch "/etc/aria2/aria2.session"
