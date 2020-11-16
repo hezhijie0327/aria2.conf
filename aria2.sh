011641663809d17d77f7603e4a13f0f872b98d4b
Zhijie He <hezhijie0327@hotmail.com>
2020-11-16 10:33:16 +0800

    aria2.sh | Version 1.0.5
    1. <Change> Cleanup dir of /etc/aria2.
    *
    /etc/aria2
        /etc/aria2/cert -> SSL certificate
        /etc/aria2/conf -> Configuration file
        /etc/aria2/data -> Download folder
        /etc/aria2/work -> Other files
    *

--- aria2.sh
+++ aria2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.0.4
+# Current Version: 1.0.5
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/aria2.conf.git" && chmod 0777 ./aria2.conf/aria2.sh && bash ./aria2.conf/aria2.sh
@@ -8,34 +8,46 @@
 aria2c_pid=$(ps -ef | grep "aria2c" | grep -v "grep" | awk '{ print $2 }')
 
 if [ "${aria2c_pid}" == "" ]; then
-    if [ ! -d "/etc/aria2/Downloads" ]; then
-        mkdir "/etc/aria2/Downloads"
+    if [ ! -d "/etc/aria2" ]; then
+        mkdir "/etc/aria2"
     fi
-    if [ ! -f "/etc/aria2/aria2.conf" ]; then
-        curl "https://source.zhijie.online/aria2.conf/master/aria2_qb_linux.conf" > "/etc/aria2/aria2.conf"
+    if [ ! -d "/etc/aria2/cert" ]; then
+        mkdir "/etc/aria2/cert"
     fi
-    if [ ! -f "/etc/aria2/aria2.cookie" ]; then
-        touch "/etc/aria2/aria2.cookie"
+    if [ ! -d "/etc/aria2/conf" ]; then
+       mkdir "/etc/aria2/conf"
     fi
-    if [ ! -f "/etc/aria2/aria2.dht" ]; then
-        touch "/etc/aria2/aria2.dht"
+    if [ ! -d "/etc/aria2/data" ]; then
+       mkdir "/etc/aria2/data"
     fi
-    if [ ! -f "/etc/aria2/aria2.dht6" ]; then
-        touch "/etc/aria2/aria2.dht6"
+    if [ ! -d "/etc/aria2/work" ]; then
+        mkdir "/etc/aria2/work"
     fi
-    if [ ! -f "/etc/aria2/aria2.log" ]; then
-        touch "/etc/aria2/aria2.log"
+    if [ ! -f "/etc/aria2/conf/aria2.conf" ]; then
+        curl "https://source.zhijie.online/aria2.conf/master/aria2_qb_linux.conf" > "/etc/aria2/conf/aria2.conf"
     fi
-    if [ ! -f "/etc/aria2/aria2.netrc" ]; then
-        touch "/etc/aria2/aria2.netrc"
+    if [ ! -f "/etc/aria2/work/aria2.cookie" ]; then
+        touch "/etc/aria2/work/aria2.cookie"
     fi
-    if [ ! -f "/etc/aria2/aria2.session" ]; then
-        touch "/etc/aria2/aria2.session"
+    if [ ! -f "/etc/aria2/work/aria2.dht" ]; then
+        touch "/etc/aria2/work/aria2.dht"
     fi
-    if [ ! -f "/etc/aria2/aria2.stat" ]; then
-        touch "/etc/aria2/aria2.stat"
+    if [ ! -f "/etc/aria2/work/aria2.dht6" ]; then
+        touch "/etc/aria2/work/aria2.dht6"
     fi
-    aria2c --conf-path="/etc/aria2/aria2.conf"
+    if [ ! -f "/etc/aria2/work/aria2.log" ]; then
+        touch "/etc/aria2/work/aria2.log"
+    fi
+    if [ ! -f "/etc/aria2/work/aria2.netrc" ]; then
+        touch "/etc/aria2/work/aria2.netrc"
+    fi
+    if [ ! -f "/etc/aria2/work/aria2.session" ]; then
+        touch "/etc/aria2/work/aria2.session"
+    fi
+    if [ ! -f "/etc/aria2/work/aria2.stat" ]; then
+        touch "/etc/aria2/work/aria2.stat"
+    fi
+    aria2c --conf-path="/etc/aria2/conf/aria2.conf"
     exit 0
 else
     kill ${aria2c_pid}
