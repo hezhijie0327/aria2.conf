3d93f8973187cbd93c7a596ad0f9c3f63f43d481
Zhijie He <hezhijie0327@hotmail.com>
2020-12-05 16:14:21 +0800

    aria2.sh | Version 1.1.6
    1. <Change> Minor code.

--- aria2.sh
+++ aria2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.1.5
+# Current Version: 1.1.6
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/aria2.conf.git" && bash ./aria2.conf/aria2.sh
@@ -26,10 +26,7 @@ if [ "${aria2c_pid}" == "" ]; then
     if [ ! -f "/etc/aria2/conf/aria2.conf" ]; then
         wget -O "/etc/aria2/conf/aria2.conf" "https://source.zhijie.online/aria2.conf/main/aria2_qb_linux.conf" && echo "$[$(date '+%s') + 86400]" > "/etc/aria2/work/aria2.exp"
     else
-        if [ ! -f "/etc/aria2/work/aria2.exp" ]; then
-            echo "$[$(date '+%s') + 86400]" > "/etc/aria2/work/aria2.exp"
-        fi
-        if [ "$(cat '/etc/aria2/work/aria2.exp')" -le "$(date '+%s')" ]; then
+        if [ ! -f "/etc/aria2/work/aria2.exp" ] || [ "$(cat '/etc/aria2/work/aria2.exp')" -le "$(date '+%s')" ]; then
             wget -qO- "https://source.zhijie.online/aria2.conf/main/aria2_qb_linux.conf" | sed "s/\#rpc\-certificate\=\/etc\/aria2\/cert\/fullchain\.pem/$(cat '/etc/aria2/conf/aria2.conf' | grep 'rpc\-certificate' | sed 's/\//\\\//g')/g;s/\#rpc\-private\-key\=\/etc\/aria2\/cert\/privkey\.pem/$(cat '/etc/aria2/conf/aria2.conf' | grep 'rpc\-private\-key' | sed 's/\//\\\//g')/g;s/\#rpc\-secret\=/$(cat '/etc/aria2/conf/aria2.conf' | grep 'rpc\-secret')/g;s/rpc\-secure\=false/$(cat '/etc/aria2/conf/aria2.conf' | grep 'rpc\-secure')/g" > "/etc/aria2/conf/aria2.conf" && echo "$[$(date '+%s') + 86400]" > "/etc/aria2/work/aria2.exp"
         fi
     fi
